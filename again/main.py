5b89b75289dbe342ac9d19ccfa95bd0bd36fa204
Himanshu <himanshukar1810@gmail.com>
2023-08-30 23:55:57 +0530

    Learned basics of BeautifulSoup by scraping a local html file

---
+++ again/main.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+
+# with open('./again/index.html', 'r') as html_file:
+#    content = html_file.read()
+#    print(content)
+#    soup = BeautifulSoup(content, 'lxml')
+#    print(soup.prettify())
+
+#    tags = soup.find_all('div')
+#    print(tags)
+#    for tag in tags:
+#        print(tag)
+
+#    div_tags = soup.find_all('div', class_='container')
+#    for div in div_tags:
+#       print(div.h3)
